a6e879923fbe981a010ca7ae9c28e8f7e79403f4
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-07-22 21:31:00 +0530

    add

---
+++ src/main/java/com/practise/Entity/Feedbacks.java
@@ -0,0 +1,52 @@
+package com.practise.Entity;
+
+public class Feedbacks {
+
+    private int id;
+    private String UserName;
+    private String email;
+    private String Comments;
+    private int rating;
+
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return UserName;
+    }
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getComments() {
+        return Comments;
+    }
+    public void setComments(String comments) {
+        Comments = comments;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    @Override
+    public String toString() {
+        return "Feedbacks [id=" + id + ", UserName=" + UserName + ", email=" + email + ", Comments=" + Comments
+                + ", rating=" + rating + "]";
+    }
+
+    // create getter and setter
+    
+    
+}
--- src/main/java/com/practise/app/App.java
+++ src/main/java/com/practise/app/App.java
@@ -1,4 +1,7 @@
-package com.practise;
+package com.practise.app;
+
+
+import com.practise.Utility.HibernateUtility;
 
 /**
  * Hello world!
@@ -9,5 +12,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        HibernateUtility.getSessionFactory();
     }
 }
